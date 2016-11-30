2bb81c1d2398038ddda8a2078c4f6f211d27231f
icrona <aldomakmur@gmail.com>
2016-11-30 16:47:17 +0700

    sprinkle piping tahap satu selesai

---
+++ Assets/Scripts/DraggerForText.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DraggerForText : MonoBehaviour
+{
+    Vector3 mousePosition;
+    Vector3 objectPosition;
+    public float height, height2;
+    public int tier;
+    public int index;
+    public float distance;
+    private GameObject deleteText;
+    private bool delete;
+    void Start()
+    {
+        tier = transform.parent.parent.parent.GetSiblingIndex();
+        delete = false;
+    }
+    void Update()
+    {
+        for (int i = 0; i < transform.parent.parent.parent.childCount; i++)
+        {
+            if (transform.parent.parent.parent.GetChild(i).gameObject.activeSelf)
+            {
+                index = i;
+            }
+        }
+    }
+    void OnMouseDown()
+    {
+        transform.parent.parent.parent.parent.parent.GetComponent<CakeRotate>().enabled = false;
+        transform.parent.parent.parent.parent.parent.GetComponent<CakeZoom>().enabled = false;
+        transform.parent.parent.parent.parent.rotation = Quaternion.Euler(90, 180, 0);
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < transform.parent.parent.parent.childCount; j++)
+            {
+                for (int k = 0; k < transform.parent.parent.parent.parent.GetChild(i).GetChild(j).GetChild(2).childCount; k++)
+                {
+                    transform.parent.parent.parent.parent.GetChild(i).GetChild(j).GetChild(2).GetChild(k).GetComponent<TextSize>().enabled = false;
+                }
+            }
+        }
+        transform.GetComponent<TextSize>().enabled = true;
+
+    }
+    void OnMouseDrag()
+    {
+        transform.parent.parent.parent.parent.parent.GetComponent<CakeRotate>().enabled = false;
+        transform.parent.parent.parent.parent.parent.GetComponent<CakeZoom>().enabled = false;
+        transform.localRotation = Quaternion.Euler(90, 180, 0);
+        height = transform.parent.parent.parent.GetChild(index).GetChild(0).GetComponent<MeshFilter>().mesh.bounds.size.y * transform.parent.parent.parent.GetChild(index).GetChild(0).localScale.y;
+        if (tier == 2)
+        {
+            height2 = transform.parent.parent.parent.parent.GetChild(1).GetChild(index).GetChild(0).GetComponent<MeshFilter>().mesh.bounds.size.y * transform.parent.parent.parent.parent.GetChild(1).GetChild(index).GetChild(0).localScale.y;
+        }
+        switch (tier)
+        {
+            case 0:
+                distance = 180f;
+                break;
+            case 1:
+                distance = 180f - height;
+                break;
+
+            case 2:
+                distance = 180f - height - height2;
+                break;
+        }
+
+        mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance - 1.5f);
+        objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objectPosition;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "DeleteText")
+        {
+            deleteText = collision.gameObject;
+            collision.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+            delete = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "DeleteText")
+        {
+            collision.transform.localScale = new Vector3(1f, 1f, 1f);
+            delete = false;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if (delete)
+        {
+            Destroy(gameObject);
+            delete = false;
+            deleteText.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/SelectSprinkle.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SelectSprinkle : MonoBehaviour {
+
+    // Use this for initialization
+    private Button[] sprinkle;
+    public GameObject sprinkles;
+    public int numOfSprinkle;
+    public int current;
+    public GameObject selectedSprinkle;
+    private int tier;
+    void Start () {
+        numOfSprinkle = sprinkles.transform.childCount;
+        selectedSprinkle = new GameObject();
+        sprinkle = new Button[numOfSprinkle];
+        current = 0;
+        for (int i = 0; i < numOfSprinkle; i++)
+        {
+            sprinkle[i] = sprinkles.transform.GetChild(i).GetComponent<Button>();
+            Button b = sprinkle[i];
+            AddListener(b, i);
+        }
+        tier = transform.GetSiblingIndex() + 1;
+    }
+
+    void AddListener(Button b, int i)
+    {
+        b.onClick.AddListener(() => pickSprinkle(b, i));
+    }
+
+    void pickSprinkle(Button b, int x)
+    {
+        for (int i = 0; i < numOfSprinkle; i++)
+        {
+            sprinkle[i].interactable = true;
+        }
+        selectedSprinkle = transform.GetChild(0).GetChild(0).GetChild(1).GetChild(1).GetChild(current).gameObject;
+        hideSprinkle();
+        b.interactable = false;
+        if (x != 0)
+        {
+            current = x-1;
+            selectedSprinkle = transform.GetChild(0).GetChild(0).GetChild(1).GetChild(1).GetChild(current).gameObject;
+            StartCoroutine(sprinkleAnimate(current));
+        }
+    }
+
+    void hideSprinkle()
+    {
+        for (int i = 0; i < selectedSprinkle.transform.childCount; i++)
+        {
+            transform.GetChild(0).GetChild(0).GetChild(1).GetChild(1).GetChild(current).GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator sprinkleAnimate(int current)
+    {
+        Debug.Log(selectedSprinkle.transform.childCount);
+        for (int i = 0; i < selectedSprinkle.transform.childCount; i++)
+        {
+            transform.GetChild(0).GetChild(0).GetChild(1).GetChild(1).GetChild(current).GetChild(i).gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
